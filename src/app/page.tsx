abce916cdf271575ac21580b4240a0679e5abb31
Astar <bahouidi99@gmail.com>
2023-12-22 16:42:16 +0100

    Card implementation from shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "@/components/ui/card";
+
 export interface Recipe {
   id: string;
   title: string;
@@ -17,6 +26,28 @@ const getRecipes = async (): Promise<Recipe[]> => {
 
 export default async function Home() {
   const recipes = await getRecipes();
-  console.log(recipes);
-  return <main>home</main>;
+
+  return (
+    <main>
+      <div className="grid grid-cols-3 gap-8">
+        {recipes.map((recipe) => (
+          <Card key={recipe.id} className="flex flex-col justify-between">
+            <CardHeader>
+              <div>
+                <CardTitle>{recipe.title}</CardTitle>
+                <CardDescription>{recipe.time} mins to cook.</CardDescription>
+              </div>
+            </CardHeader>
+            <CardContent>
+              <p>{recipe.description}</p>
+            </CardContent>
+            <CardFooter className="flex justify-between">
+              <button>View Recipe</button>
+              {recipe.vegan && <p className="mt-0">Vegan!</p>}
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
+    </main>
+  );
 }

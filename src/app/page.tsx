fd29ced9a72363e0780e7eed8840be3933d08fef
Astar <bahouidi99@gmail.com>
2023-12-22 15:30:18 +0100

    Fetch recipes from db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
-export default function Home() {
-  return (
-    <main>
-      home
-    </main>
-  )
+export interface Recipe {
+  id: string;
+  title: string;
+  image: string;
+  time: number;
+  description: string;
+  vegan: boolean;
+}
+
+const getRecipes = async (): Promise<Recipe[]> => {
+  const response = await fetch("http://localhost:4000/recipes");
+  if (!response.ok) {
+    throw new Error("Something went wrong");
+  }
+  return response.json();
+};
+
+export default async function Home() {
+  const recipes = await getRecipes();
+  console.log(recipes);
+  return <main>home</main>;
 }
